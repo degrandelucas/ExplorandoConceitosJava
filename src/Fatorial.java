463922d4181880f39ff0c4898487d5c017b366a1
degrandelucas <lucasdegrande15@gmail.com>
2024-11-23 16:31:40 -0300

    implantação da classe Fatorial com o metodo de calculo fatorial de um numero

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Fatorial {
     public static void NumeroFatorial() {
-
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("----------------------------------------------");
+        System.out.println("Digite o número para cálculo fatorial");
+        int numero = leitura.nextInt();
+        int fatorial = 1;
+        for (int i = 1; i <= numero; i++) {
+            fatorial = fatorial * i;
+        }
+        System.out.println(String.format("o fatorial do número %d é %d.", numero, fatorial));
     }
 }
